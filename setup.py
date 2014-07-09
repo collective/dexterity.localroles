f745da5169adb8c02b02e1db75dac45244e935a3
Martin Peeters <martin.peeters@affinitic.be>
2014-07-09 09:39:18 +0200

    Pep8 fix

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 from setuptools import setup, find_packages
-import os
 
 version = '0.1'
 
@@ -14,13 +13,14 @@ long_description = (
     open('CHANGES.rst').read()
     + '\n')
 
-setup(name='dexterity.localroles',
-      version=version,
-      description="Define local roles settings by dexterity type",
-      long_description=long_description,
-      # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
+setup(
+    name='dexterity.localroles',
+    version=version,
+    description="Define local roles settings by dexterity type",
+    long_description=long_description,
+    # Get more strings from
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
         "Environment :: Web Environment",
         "Framework :: Plone",
         "Framework :: Plone :: 4.3",
@@ -28,25 +28,25 @@ setup(name='dexterity.localroles',
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
         "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
-      keywords='',
-      author='IMIO',
-      author_email='support@imio.be',
-      url='https://github.com/imio/',
-      license='gpl',
-      packages=find_packages('src'),
-      package_dir={'': 'src'},
-      namespace_packages=['dexterity', ],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          # -*- Extra requirements: -*-
-      ],
-      extras_require={'test': ['plone.app.testing']},
-      entry_points="""
-      # -*- Entry points: -*-
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      )
+    ],
+    keywords='',
+    author='IMIO',
+    author_email='support@imio.be',
+    url='https://github.com/imio/',
+    license='gpl',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['dexterity', ],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        # -*- Extra requirements: -*-
+    ],
+    extras_require={'test': ['plone.app.testing']},
+    entry_points="""
+    # -*- Entry points: -*-
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
