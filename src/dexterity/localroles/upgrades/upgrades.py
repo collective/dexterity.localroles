d097141730e2963f9b079f578404340f10aef1f8
Stephan Geulette <s.geulette@imio.be>
2015-10-28 22:36:49 +0100

    Added migration step

---
+++ src/dexterity/localroles/upgrades/upgrades.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import logging
+
+from zope.component import getUtilitiesFor
+
+from Products.CMFPlone.utils import base_hasattr
+from plone.dexterity.interfaces import IDexterityFTI
+
+logger = logging.getLogger('dexterity.localroles: upgrade. ')
+
+
+def v2(context):
+    for (name, fti) in getUtilitiesFor(IDexterityFTI):
+        if not base_hasattr(fti, 'localroleconfig'):
+            continue
+        logger.info("Copying old config to static_config: '%s'" % fti.localroleconfig)
+        if not base_hasattr(fti, 'localroles'):
+            setattr(fti, 'localroles', {})
+        fti.localroles['static_config'] = fti.localroleconfig
+        delattr(fti, 'localroleconfig')
