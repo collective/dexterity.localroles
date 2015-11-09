f7ff42b5faf5f347f4bff150ab8718a90419b85b
Stephan Geulette <s.geulette@imio.be>
2015-11-09 09:07:03 +0100

    Moved in a method the code for related role removal

--- src/dexterity/localroles/subscriber.py
+++ src/dexterity/localroles/subscriber.py
@@ -20,15 +20,10 @@ def local_role_configuration_updated(context, event):
         obj.reindexObjectSecurity()
 
 
-def related_change_on_transition(context, event):
-    """ Set local roles on related objects after transition """
-    fti_config = fti_configuration(context)
-    if 'static_config' not in fti_config:
-        return
-    uid = context.UID()
-    # We have to remove the configuration linked to old state
-    if event.old_state.id != event.new_state.id and event.old_state.id in fti_config['static_config']:
-        dic = fti_config['static_config'][event.old_state.id]
+def related_role_removal(context, state, fti_config):
+    if state in fti_config['static_config']:
+        dic = fti_config['static_config'][state]
+        uid = context.UID()
         for princ in dic:
             if dic[princ].get('rel', ''):
                 related = eval(dic[princ]['rel'])
@@ -36,8 +31,19 @@ def related_change_on_transition(context, event):
                     for obj in runRelatedSearch(rel_dic['utility'], context):
                         if del_related_roles(obj, uid):
                             obj.reindexObjectSecurity()
+
+
+def related_change_on_transition(context, event):
+    """ Set local roles on related objects after transition """
+    fti_config = fti_configuration(context)
+    if 'static_config' not in fti_config:
+        return
+    # We have to remove the configuration linked to old state
+    if event.old_state.id != event.new_state.id:
+        related_role_removal(context, event.old_state.id, fti_config)
     # We have to add the configuration linked to new state
     if event.new_state.id in fti_config['static_config']:
+        uid = context.UID()
         dic = fti_config['static_config'][event.new_state.id]
         for princ in dic:
             if dic[princ].get('rel', ''):
@@ -55,18 +61,8 @@ def related_change_on_removal(context, event):
     fti_config = fti_configuration(context)
     if 'static_config' not in fti_config:
         return
-    uid = context.UID()
-    state = get_state(context)
     # We have to remove the configuration linked to deleted object
     # There is a problem in Plone 4.3. The event is notified before the confirmation and after too.
     # The action could be cancelled: we can't know this !! Resolved in Plone 5...
     # We choose to update related objects anyway !!
-    if state in fti_config['static_config']:
-        dic = fti_config['static_config'][state]
-        for princ in dic:
-            if dic[princ].get('rel', ''):
-                related = eval(dic[princ]['rel'])
-                for rel_dic in related:
-                    for obj in runRelatedSearch(rel_dic['utility'], context):
-                        if del_related_roles(obj, uid):
-                            obj.reindexObjectSecurity()
+    related_role_removal(context, get_state(context), fti_config)
